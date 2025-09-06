2e7dde9f4987d74effc04707c73368cf47bf35f8
yareach8345 <mine8345@naver.com>
2025-09-06 23:26:30 +0900

    feature - CustomUserDetail을 Backend 프로젝트에서 Common으로 옮김

---
+++ src/main/kotlin/com/yareach/socketjamcommon/config/security/CustomUserDetail.kt
@@ -0,0 +1,32 @@
+package com.yareach.socketjamcommon.config.security
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import kotlin.collections.map
+
+class CustomUserDetail(
+    val userId: String,
+    val nickName: String,
+    val role: List<String>
+): UserDetails {
+    override fun getAuthorities(): Collection<GrantedAuthority?> {
+        return role.map { SimpleGrantedAuthority(it) }
+    }
+
+    override fun getPassword(): String {
+        return ""
+    }
+
+    override fun getUsername(): String {
+        return userId
+    }
+
+    fun getNickName(): String {
+        return nickName
+    }
+
+    fun getUserId(): String {
+        return userId
+    }
+}
